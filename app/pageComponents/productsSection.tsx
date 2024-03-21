3b5f6f5d16aa7a6db462d6a1c755ce2643a684f9
maramzan <arsalanramzan811@gmail.com>
2024-03-22 02:39:06 +0500

    products rendering and add more products done

--- app/pageComponents/productsSection.tsx
+++ app/pageComponents/productsSection.tsx
@@ -9,11 +9,9 @@ const ProductsSection = () => {
   const [products, setProducts] = useState<object[]>([]);
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
 
   const getProducts = async () => {
     try {
-      setLoading(true);
       const limit = 10;
       const skip = (page - 1) * limit;
       const response = await axios.get(
@@ -25,8 +23,6 @@ const ProductsSection = () => {
       setHasMore(response.data.total > skip + limit);
     } catch (error) {
       console.error("Error fetching products:", error);
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -53,34 +49,14 @@ const ProductsSection = () => {
           mt: 5,
         }}
       >
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <>
-            {products.map((product, index) => (
-              <ProductCard key={index} product={product} />
-            ))}
-          </>
-        )}
-
-        {/* {products.map((product, index) => (
+        {products.map((product, index) => (
           <ProductCard key={index} product={product} />
-        ))} */}
-        {/* {true && <CircularProgress />} */}
-        {/* {hasMore && products?.length ? (
+        ))}
+        {hasMore && products?.length ? (
           <Button sx={{ mt: 5 }} onClick={fetchMoreProducts} variant="outlined">
             Load More Products
           </Button>
-        ) : (
-          <Typography
-            variant="h6"
-            fontWeight="regular"
-            color="text.secondary"
-            mt={5}
-          >
-            No products to load...
-          </Typography>
-        )} */}
+        ) : null}
       </Box>
     </Box>
   );
