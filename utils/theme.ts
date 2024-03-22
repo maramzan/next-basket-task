6cc0b258ede67c25c3c89a7ef857af12bfa460df
maramzan <arsalanramzan811@gmail.com>
2024-03-23 00:15:13 +0500

    font removed

--- utils/theme.ts
+++ utils/theme.ts
@@ -22,9 +22,9 @@ const theme = createTheme({
       default: "#fff",
     },
   },
-  typography: {
-    fontFamily: "Montserrat, sans-serif",
-  },
+  // typography: {
+  //   fontFamily: "Montserrat, sans-serif",
+  // },
 });
 
 export default theme;
